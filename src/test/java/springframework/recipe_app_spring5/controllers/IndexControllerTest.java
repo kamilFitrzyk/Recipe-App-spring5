6a525d7b0e3826523a4bf81ff77caa0a4165085f
kamilFitrzyk <kamilfitrzyk89@gmail.com>
2022-03-05 23:59:11 +0100

    indexControllerTest

---
+++ src/test/java/springframework/recipe_app_spring5/controllers/IndexControllerTest.java
@@ -0,0 +1,12 @@
+package springframework.recipe_app_spring5.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    @Test
+    void getIndexPage() {
+    }
+}
