777ac0c9d86b231ee6f68418096a13f5962624cf
kamilFitrzyk <kamilfitrzyk89@gmail.com>
2022-04-29 23:18:32 +0200

    change application.properties

--- databaseCreate.sql
+++ databaseCreate.sql
@@ -10,3 +10,15 @@ alter table notes add constraint notuni foreign key (recipe_id) references recip
 alter table recipe add constraint recnot foreign key (notes_id) references notes (id);
 alter table recipe_category add constraint reccat foreign key (category_id) references category (id);
 alter table recipe_category add constraint reccrec foreign key (recipe_id) references recipe (id);
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+INSERT INTO unit_of_measure (description) VALUES ('Each');
+INSERT INTO unit_of_measure (description) VALUES ('Dash');
+INSERT INTO unit_of_measure (description) VALUES ('Pint');
